fee29b17d73ec5e0114d42d1d6d3f60449d8107e
Zane Pollard <zane.pollard@thesitecontroller.local>
2018-06-27 11:17:06 -0500

    outputs properly to pt.dat file

--- ptgen.py
+++ ptgen.py
@@ -17,7 +17,7 @@ OID = "0" #odometer implied decimal
 pump = [] #2 char from variables file
 tranNum = [] #4 char from variable file ***DONE***
 tranDate = [] #6 chars YYMMDD ***DONE***
-tranTime = [] #4 char HHNN (military time)
+tranTime = [] #4 char HHNN (military time) ***DONE***
 fill = "00000000"
 id_vehicle = [] #8 char from variable file name is id_vehicle
 id_card = [] #7 char from variable file
@@ -25,10 +25,15 @@ part_id ="000"
 id_acct = [] #6 chars from variable file of the same name
 vehicle = [] #4 chars, same as id_vehicle but without leading zeros
 end = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000DCF000000000000000000000000"
+fileDate = ""
 
 #Formats date in the proper PT file format
+def tFormat(time):
+    return (time[0:2]+time[3:5])
+
 def dFormat(date):
     fDate = date[8:10] + date[0:2] + date[3:5]
+    filedate = date[0:2] + date[3:5] + date[8:10]
     return fDate
 
 def decimalSplit(number, x, y): #y is decimal t/f
@@ -71,6 +76,12 @@ def decimalCheck(number, x):
     else:
         decimalSplit(number, x, False)
 
+def format(oD, num):
+    temp = oD
+    for i in range(num-len(oD)):
+        temp = "0"+temp
+    return temp
+
 def ptGen():
     runCount = 0
     firstRun = True
@@ -81,6 +92,7 @@ def ptGen():
         if fnmatch.fnmatch(file,'}h*.d1c'):
             filename = file
     with open(filename, newline='') as csvfile:
+        
         reader = csv.reader(csvfile, quotechar="\"")
         
         for row in reader:
@@ -105,9 +117,32 @@ def ptGen():
                     if re.search(dPattern, drow[3]):
                         decimalCheck(drow[10], True) #True = Quantity
                         decimalCheck(drow[48], False)
-                        pCode.append(drow[11])               
+                        pCode.append(drow[11])
+                        tranTime.append(tFormat(drow[2]))
+            for file in os.listdir('.'):
+                if fnmatch.fnmatch(file,'}v*.d1c'):
+                    VFile = file    
+            with open(VFile, newline='') as csvfile:
+                vreader = csv.reader(csvfile, quotechar="\"") 
+                for vrow in vreader:
+                    if tranNum[runCount] == vrow[3][1:5]:
+                        if vrow[8] == "SEQUENCE#":
+                            seqnum.append(vrow[9])
+                        elif vrow[8] == "ODOMETER":
+                            odometer.append(format(vrow[9],7))
+                        elif vrow[8] == "pump":
+                            pump.append(format(vrow[9],2))
+                        elif vrow[8] == "ID_VEHICLE":
+                            id_vehicle.append(format(vrow[9],8))
+                        elif vrow[8] == "ID_CARD":
+                            id_card.append(format(vrow[9],7))
+                        elif vrow[8] == "ID_ACCT":
+                            id_acct.append(format(vrow[9],6))
+                        elif vrow[8] == "VEHICLE":
+                            vehicle.append(format(vrow[9],4))               
             runCount+=1
+        f= open("testOutput.dat","w+")
         for i in range(runCount):
-            print(siteid+"9999"+totAmt[i]+ACT+TRANTYPE+pCode[i]+price+quantity[i]) #test line
-    #print(siteid)
+            f.write(siteid+seqnum[i]+STATCODE+totAmt[i]+ACT+TRANTYPE+pCode[i]+price+quantity[i]+odometer[i]+OID+pump[i]+tranNum[i]+tranDate+tranTime[i]+fill+id_vehicle[i]+id_card[i]+part_id+id_acct[i]+vehicle[i]+end+"\n")
+
 ptGen()
