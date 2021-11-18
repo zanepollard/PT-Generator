28a9cf920f95671c0b996473784f7f5b980d9a1c
zanepollard <pollardzane@gmail.com>
2021-11-18 10:12:23 -0600

    using the new python 3.10 'match' statements

--- salesgen.py
+++ salesgen.py
@@ -43,26 +43,25 @@ def main():
     pullMode = config_data.get('pullMode')
     SOFTWARE_VERSION = config_data.get('SOFTWARE_VERSION')
 
-    if(SOFTWARE_VERSION == "VB6" or SOFTWARE_VERSION == ".NET"):
-        print(f"Software Version: {SOFTWARE_VERSION}")
-
-        if headless:
-            generateSales(config_data,
-                          SOFTWARE_VERSION,
-                          pullFiles(config_data,pullMode,input_folders,SOFTWARE_VERSION,dateRun,lastRunDate,log_file),
-                          root,
-                          output_folder,
-                          log_file)
-        else:
-            #TODO for CLI
-            pass
-        
-        transfer(config_data, output_folder)
-        files.set_lastRun_Date(dateRun, root)    
-    else:   
-        print(f"Please check config. Software version set to {SOFTWARE_VERSION}, should be either '.NET' or 'VB6'")
-        files.log_events(log_file,f"{dateRun}:  ERROR: incorrect software version configuration\n")
-        exit(1)
+    match SOFTWARE_VERSION:
+        case ("VB6"|".NET"):
+            print(f"Software Version: {SOFTWARE_VERSION}")
+            match headless:
+                case True:
+                    generateSales(config_data,
+                            SOFTWARE_VERSION,
+                            pullFiles(config_data,pullMode,input_folders,SOFTWARE_VERSION,dateRun,lastRunDate,log_file),
+                            root,
+                            output_folder,
+                            log_file)
+                case False:
+                    pass
+            transfer(config_data, output_folder)
+            files.set_lastRun_Date(dateRun, root)  
+        case _:
+            print(f"Please check config. Software version set to {SOFTWARE_VERSION}, should be either '.NET' or 'VB6'")
+            files.log_events(log_file,f"{dateRun}:  ERROR: incorrect software version configuration\n")
+            exit(1)
 
 
 def userControl():
@@ -75,7 +74,7 @@ def userControl():
         table.add_row("3", "Export all data to sales file")
 
         console.print(table)
-        selection = Prompt.ask("Enter your selection", choices=["1","2"],show_choices=None, console=console)
+        selection = Prompt.ask("Enter your selection", choices=["1","2","3"],show_choices=None, console=console)
         break
 
 
@@ -83,20 +82,25 @@ def pullFiles(config_data, pullMode, input_Folders, SOFTWARE_VERSION, dateRun, l
     fileList = []
     for folderPath in input_Folders:
         input_folder = os.path.abspath(folderPath)
-        if(pullMode == "ALL"):
-            fileList = fileList + files.fileFind(input_folder,config_data,SOFTWARE_VERSION,pullMode)
-        elif(pullMode == "DAILY"):
-            timeDelta = (dateRun-(lastRunDate)).days
-            if((timeDelta <= 1)):
-                fileList = fileList + files.fileFind(input_folder,config_data,SOFTWARE_VERSION,pullMode,(dateRun-timedelta(days=1)))
-            elif(timeDelta > 1):
-                #Case when program hasn't been run in a while. Looking for any files during that time.
-                fileList = fileList + files.fileFind(input_folder,config_data,SOFTWARE_VERSION,"RANGE",(dateRun-timedelta(days=1)),lastRunDate)
-        elif(pullMode == "RANGE"):
-            #implement later alongside command line interface.
-            pass
+        match pullMode:
+            case "ALL":
+                fileList = fileList + files.fileFind(input_folder,config_data,SOFTWARE_VERSION,pullMode)
+            case "DAILY":
+                timeDelta = (dateRun-(lastRunDate)).days
+                if((timeDelta <= 1)):
+                    fileList = fileList + files.fileFind(input_folder,config_data,SOFTWARE_VERSION,pullMode,(dateRun-timedelta(days=1)))
+                elif(timeDelta > 1):
+                    #Case when program hasn't been run in a while. Looking for any files during that time.
+                    fileList = fileList + files.fileFind(input_folder,config_data,SOFTWARE_VERSION,"RANGE",(dateRun-timedelta(days=1)),lastRunDate)
+            case "RANGE":
+                #implement later alongside command line interface.
+                pass
+            case _:
+                console.print(f"ERROR: Check pullMode setting in config. Must be set to 'ALL', 'DAILY', or 'RANGE'. Currently set to '{pullMode}'\n")
+                files.log_events(log_file,f"{dateRun}:  ERROR: Check pullMode setting in config. Must be set to 'ALL', 'DAILY', or 'RANGE'. Currently set to '{pullMode}'\n")
+                exit(1)
     if(len(fileList) == 0):
-        print(f"No files found to parse matching date parameters in '{input_folder}'\n")
+        console.print(f"ERROR: no files found matching date parameters in '{input_folder}'\n")
         files.log_events(log_file,f"{dateRun}:  ERROR: no files found matching date parameters in '{input_folder}'\n")
         exit(1)
     else:
@@ -104,15 +108,16 @@ def pullFiles(config_data, pullMode, input_Folders, SOFTWARE_VERSION, dateRun, l
     
 
 def generateSales(config_data, SOFTWARE_VERSION, fileList, root, output_folder, log_file):
-    if(SOFTWARE_VERSION == "VB6"):
-        parseObj = VBparse.parse()
-        for x in range(len(fileList[0])):
-            parseObj.parse([fileList[0][x],fileList[1][x],fileList[2][x]])
-    elif(SOFTWARE_VERSION == ".NET"):
-        parseObj = NETparse.parse()
-        for file in fileList:
-            parseObj.parse(file)
-    
+    match SOFTWARE_VERSION:
+        case "VB6":
+            parseObj = VBparse.parse()
+            for x in range(len(fileList[0])):
+                parseObj.parse([fileList[0][x],fileList[1][x],fileList[2][x]])
+        case ".NET":
+            parseObj = NETparse.parse()
+            for file in fileList:
+                parseObj.parse(file)
+
     if(config_data.get('singleFile')):
         files.salesOutput_ind(parseObj, config_data, root, output_folder)
     else:
